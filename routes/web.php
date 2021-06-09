24fed39369e841e0cb096c7a8909fa301a2409ec
Lucas Hart <lucashart.it@gmail.com>
2021-06-09 13:15:32 +0100

    Removed route names

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('payment/{id}', 'PaymentController@show')->name('payment');
-Route::post('webhook', 'WebhookController@handleWebhook')->name('webhook');
+Route::get('payment/{id}', 'PaymentController@show');
+Route::post('webhook', 'WebhookController@handleWebhook');
+
